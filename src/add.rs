436d7420ffc98ccb8ca3a48acde6a845905f403d
Ruslan Basyrov <rusruo@gmail.com>
2025-01-28 01:09:33 +0100

    ex00: comments and tests

--- src/add.rs
+++ src/add.rs
@@ -1,3 +1,8 @@
+// The task of the exercise is to write a function,
+// which takes two positive numbers and returns their sum.
+// Since there may be two definitions of positive numbers (with or without 0),
+// I could choose the one with less limitations: with 0.
+
 pub fn add(mut a: u32, mut b: u32) -> u32 { 
     let mut carry: u32;
     while b != 0 {
@@ -16,6 +21,22 @@ mod tests {
     fn add_small_positive_numbers() {
         let a = 2;
         let b = 10;
+        let result = add(a, b);
+        assert_eq!(result, a + b);
+    }
+
+    #[test]
+    fn add_null_and_positive() {
+        let a = 0;
+        let b = 481;
+        let result = add(a, b);
+        assert_eq!(result, a + b);
+    }
+
+    #[test]
+    fn add_null_and_max_unsigned() {
+        let a = u32::MAX;
+        let b = 0;
         let result = add(a, b);
         assert_eq!(result, a + b);
     }
